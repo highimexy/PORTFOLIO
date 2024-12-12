ed33fa4a84b5c7115cf8f8d891ef29c5b476ec25
exy <jjuicyn@proton.me>
2024-12-12 20:49:58 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +9,30 @@ export default {
   ],
   theme: {
     extend: {
+
+       
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#1c1c22',
+        accent: {
+          DEFAULT: '#00ff99',
+          hover: '#00e187'
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      fontFamily: {
+        primary: "var(--font-jetbrainsMono)", // Corrected key
       },
     },
   },
-  plugins: [],
+  screens: {
+    sm: '640px',
+    md: '768px',
+    lg: '960px',
+    xl: '1200px',
+  },
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
